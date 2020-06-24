ce669abbc5c916d5ece88d95bd9dcb167ae25c0e
AshimaKamra <akkamra18@gmail.com>
2020-06-24 17:11:31 +0530

    useState and useEffect implementation

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,18 +1,49 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import EntryForm from './EntryForm';
 import EntryList from './EntryList';
 
 
 const Entry=()=>{
     const [userEntry,setUserEntry]=useState([]);
+
+    useEffect(()=>{
+        fetch('https://to-do-application-c601d.firebaseio.com/entry.json')
+        .then(response=>response.json())
+        .then(responseData=>{
+            const loadedEntry=[];
+            for(const key in responseData)
+            {
+                loadedEntry.push({
+                    id:key,
+                    name:responseData[key].name,
+                    job:responseData[key].job
+                });
+            }
+            setUserEntry(loadedEntry);
+        })
+    },[]);
+
     const addEntryHandler=entry=>{
-        setUserEntry(prevEntry=>[...prevEntry,{id:Math.random(),...entry}])
+        fetch('https://to-do-application-c601d.firebaseio.com/entry.json',{
+        method:'POST',
+        body: JSON.stringify(entry),
+        headers:{'Content-type':'application/json'}
+    }).then(response=>{
+        return response.json();
+    }).then(responseData=>{
+        setUserEntry(prevEntry=>[
+        ...prevEntry,{id:responseData.name,...entry}])
+    });
     }
   
     const removeEntryHandler=entryId=>{
-      setUserEntry(prevEntry=>
-        prevEntry.filter(entry=>entry.id!==entryId)
-        );
+        fetch(`https://to-do-application-c601d.firebaseio.com/entry/${entryId}.json`,{
+        method:'DELETE',
+    }).then(response=>{
+        setUserEntry(prevEntry=>
+            prevEntry.filter(entry=>entry.id!==entryId)
+            );
+    })
     };
     return(
         <div className="App">
